8097a112bd0fc48771435d6fdd438d4dfb68192e
Truc Duong <trucdg@bu.edu>
2023-03-17 14:47:14 -0400

    add a set of state variables to keep track of the inputs that users will be asked to specify for the screenshot

--- screen-cap/src/App.jsx
+++ screen-cap/src/App.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
 
 import "./App.css";
 
 function App() {
+  const [inputs, setInpust] = useState({
+    url: "",
+    format: "",
+    no_ads: "",
+    no_cookie_banners: "",
+    width: "",
+    height: "",
+  });
+
   return (
     <div className="App">
       <h1>Hello</h1>
