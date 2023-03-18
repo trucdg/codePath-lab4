682ff3beeee950c4080683ee0e9658fb2c91b309
Truc Duong <trucdg@bu.edu>
2023-03-17 20:09:36 -0400

    add a function to create query string to send to the API, and call it in the submitForm function

--- screen-cap/src/App.jsx
+++ screen-cap/src/App.jsx
@@ -13,7 +13,34 @@ function App() {
     height: "",
   });
 
-  const submitForm = () => {};
+  const submitForm = () => {
+    let defaultValues = {
+      format: "jpeg",
+      no_ads: "true",
+      no_cookie_banners: "true",
+      width: "1920",
+      height: "1080",
+    };
+    if (inputs.url == "" || inputs.url == " ") {
+      alert("You forgot to submit an url!");
+    } else {
+      for (const [key, value] of Object.entries(inputs)) {
+        if (value == "") {
+          inputs[key] = defaultValues[key];
+        }
+      }
+      makeQuery();
+    }
+  };
+
+  const makeQuery = () => {
+    let wait_until = "network_idle";
+    let response_type = "json";
+    let fail_on_status = "400%2C404%2C500-511";
+    let url_starter = "https://";
+    let fullURL = url_starter + inputs.url;
+    let query = `https://api.apiflash.com/v1/urltoimage?access_key=${ACCESS_KEY}&url=${fullURL}&format=${inputs.format}&width=${inputs.width}&height=${inputs.height}&no_cookie_banners=${inputs.no_cookie_banners}&no_ads=${inputs.no_ads}&wait_until=${wait_until}&response_type=${response_type}&fail_on_status=${fail_on_status}`;
+  };
 
   return (
     <div className="whole-page">
